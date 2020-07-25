73eacbee8b94ccde16849f3fc3e2bf692c0b7cde
hyun0210 <68387798+hyun0210@users.noreply.github.com>
2020-07-25 18:40:48 +0900

    3주차 숙제

--- homework03.py
+++ homework03.py
@@ -10,8 +10,8 @@ soup = BeautifulSoup(data.text, 'html.parser')
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
 for tr in trs:
-    ranks = tr.select_one('td.number').text[0:2].strip()
+    rank = tr.select_one('td.number').text[0:2].strip()
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
-    print(ranks, title, artist)
+    print(rank, title, artist)
 
